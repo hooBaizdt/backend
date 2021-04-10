037d4ed891162e29910df5288310e0e216d4adad
hooBaizdt <dengman1966@126.com>
2021-04-10 18:37:15 +0800

    单元测试

--- tests/Feature/Api/AuthControllerTest.php
+++ tests/Feature/Api/AuthControllerTest.php
@@ -8,21 +8,40 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthControllerTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    // 插入模拟用户
+    public function testInsertMockUser()
     {
-        $this->assertTrue(true);
+        $name = 'awks';
+        $email = 'rubymay21s@gmail.com';
+        $password = '123456';
+
+        \DB::table('users')->insert([
+            'name' => $name,
+            'email' => $email,
+            'password' => \Hash::make($password),
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now(),
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => $name,
+            'email' => $email,
+            'password' => \Hash::make($password)
+        ]);
+
+        return compact('name', 'email', 'password');
     }
 
-    public function testLogin()
+    /**
+     * @param array $user
+     * @depends testInsertMockUser
+     * @return mixed
+     */
+    public function testLogin($user)
     {
         $testResponse = $this->postJson('/api/auth/login', [
-           'email' => '916809498@qq.com',
-           'password' => 123456,
+           'email' => $user['email'],
+           'password' => $user['password'],
         ]);
 
         $testResponse->assertJson([
@@ -32,6 +51,63 @@ class AuthControllerTest extends TestCase
         ]);
 
         $json = $testResponse->json();
-        $this->assertTrue($json['access_token'] === \Cache::tags('tokens')->get('916809498@qq.com'));
+        $this->assertTrue($json['access_token'] === \Cache::tags('tokens')->get($user['email']));
+
+        return $json['access_token'];
+    }
+
+    /**
+     * @param string $access_token
+     * @depends testLogin
+     * @return string
+     */
+    public function testMe(string $access_token)
+    {
+        $response = $this->postJson('/api/auth/me', [], [
+            'Authorization' => 'Bearer ' . $access_token
+        ]);
+        $response->assertJson([
+            'name' => true,
+            'email' => true,
+        ]);
+
+        return $access_token;
+    }
+
+    /**
+     * @param string $access_token
+     * @depends testMe
+     * @return string
+     */
+    public function testRefresh(string $access_token)
+    {
+        $response = $this->postJson('/api/auth/refresh', [], [
+            'Authorization' => 'Bearer ' . $access_token
+        ]);
+
+        $response->assertJson([
+            'access_token' => true,
+            'token_type' => true,
+            'expires_in' => true,
+        ]);
+
+        return $response->json()['access_token'];
+    }
+
+    /**
+     * @param string $access_token
+     * @depends testRefresh
+     */
+    public function testLogout(string $access_token)
+    {
+        $response = $this->postJson('/api/auth/logout', [], [
+            'Authorization' => 'Bearer ' . $access_token
+        ]);
+
+        $response->assertJson([
+            'message' => true,
+        ]);
+
+//        \DB::table('users')->where('email', $this->user['email'])->delete();
     }
 }
